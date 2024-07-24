fc2f9504ac114b88aa381ec59251e564be630c60
alirezaheidari-cs <alirezaheidari.cs@gmail.com>
2024-07-24 15:00:28 +0330

    bug fix

--- ftgo_utils/validation/__init__.py
+++ ftgo_utils/validation/__init__.py
@@ -3,9 +3,6 @@ from .validator_fns import(
     validate_phone_number,
     validate_password,
     validate_enum_value,
-    validate_accuracy,
-    validate_speed,
-    validate_status,
 )
 
 __all__ = ['validate_email', 'validate_phone_number', 'validate_password', 'validate_enum_value']

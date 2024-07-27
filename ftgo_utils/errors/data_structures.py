71fa86afc607545a08fd66ee99699b132b1f06ec
alirezaheidari-cs <alirezaheidari.cs@gmail.com>
2024-07-27 16:09:23 +0330

    simplified error code

--- ftgo_utils/errors/data_structures.py
+++ ftgo_utils/errors/data_structures.py
@@ -1,8 +1,8 @@
 import json
 from enum import Enum
-from typing import Optional, Union
+from typing import Optional
 
-from pydantic import BaseModel, Field, field_validator
+from pydantic import BaseModel, Field
 
 class ErrorCategory(str, Enum):
     CLIENT_ERROR = 'client_error'
@@ -18,36 +18,14 @@ class ErrorCategory(str, Enum):
     RESOURCE_LIMIT_ERROR = 'resource_limit_error'
     GENERIC_ERROR = 'generic_error'
 
-
 class ErrorCode(BaseModel):
     value: str = Field(...)
-    category: Optional[Union[ErrorCategory, str]] = Field(default=ErrorCategory.GENERIC_ERROR.value)
+    category: Optional[ErrorCategory] = Field(default=ErrorCategory.GENERIC_ERROR)
     status_code: Optional[int] = Field(None)
-    description: Optional[str] = Field(None, alias='message')
+    description: Optional[str] = Field(None)
 
     class Config:
         frozen = True
-
-    @field_validator('category', mode='before')
-    def validate_category_field(cls, value):
-        if not value:
-            return ErrorCategory.GENERIC_ERROR
-        if isinstance(value, Enum):
-            value = value.value
-
-        if not isinstance(value, str):
-            raise ValueError("Invalid category type")
-
-        if value not in {category.value for category in ErrorCategory}:
-            raise ValueError("Invalid category value")
-
-        return value
-
-    def __str__(self) -> str:
-        return self.to_json()
-
-    def __repr__(self) -> str:
-        return f"{self.__class__.__name__}({self.to_json()})"
 
     def to_dict(self) -> dict:
         return self.dict(exclude_none=True)

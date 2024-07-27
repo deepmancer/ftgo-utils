019c288941396675e2c37d2dd62ad652d18fea14
alirezaheidari-cs <alirezaheidari.cs@gmail.com>
2024-07-27 12:36:12 +0330

    improved baseerror

--- ftgo_utils/errors/base.py
+++ ftgo_utils/errors/base.py
@@ -2,24 +2,25 @@ import json
 from typing import Optional, Dict, Any
 import uuid
 
-
 class BaseError(Exception):
     def __init__(
         self, 
         error_code: str = "GENERIC_ERROR", 
         payload: Optional[Dict[str, Any]] = None,
         message: Optional[str] = None, 
-        *args, 
-        **kwargs,
+        **kwargs
     ):
-        super().__init__(message, *args, **kwargs)
-        self.error_id = str(uuid.uuid4())
+        super().__init__(error_code)
+        self.error_id = uuid.uuid4().hex
         self.error_code = error_code
         self.payload = payload or {}
         self.message = message
 
-    def __str__(self):
-        return json.dumps(self.to_dict(), default=str, indent=4)
+    def __str__(self) -> str:
+        return self.to_json()
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.to_json()})"
 
     def to_dict(self) -> Dict[str, Any]:
         error_details = {

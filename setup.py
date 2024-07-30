5bc763c469f4e843ce81003c4c0ab0ff4a2369c5
alirezaheidari-cs <alirezaheidari.cs@gmail.com>
2024-07-30 14:47:00 +0330

    hot fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ftgo_utils",
-    version="1.11.1",
+    version="1.11.2",
     description="Shared utilities for FTGO platform",
     author="Alireza Heidari",
     author_email="alirezaheidari.cs@gmail.com",
